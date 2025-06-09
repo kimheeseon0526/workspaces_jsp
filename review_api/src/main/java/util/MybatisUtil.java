687354034095679812dd0b253caed9ac87706277
kimheeseon0526 <khseon0526@naver.com>
2025-06-09 18:17:45 +0900

    0609

---
+++ review_api/src/main/java/util/MybatisUtil.java
@@ -0,0 +1,38 @@
+package util;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.mapping.Environment;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.apache.ibatis.transaction.TransactionFactory;
+import org.apache.ibatis.transaction.jdbc.JdbcTransactionFactory;
+
+public class MybatisUtil {
+	
+	public static void main(String[] args) {
+		System.out.println(sqlSessionFactory.getConfiguration().getEnvironment().getDataSource());
+		// 여기서부터 세로운 내용
+		System.out.println(getSqlSession());
+	}
+	
+	private static SqlSessionFactory sqlSessionFactory;
+	
+	static {
+		InputStream is;
+		try {
+			is = Resources.getResourceAsStream("mybatis-config.xml");
+			sqlSessionFactory = new SqlSessionFactoryBuilder().build(is);
+			TransactionFactory factory = new JdbcTransactionFactory();
+			Environment environment = new Environment("mybatis-conf", factory, hikariCPUtil.getDataSource());
+			sqlSessionFactory.getConfiguration().setEnvironment(environment);
+		} catch (IOException e) {e.printStackTrace();}
+	}
+
+	public static SqlSession getSqlSession() {
+		return sqlSessionFactory.openSession(true);
+	}
+}
